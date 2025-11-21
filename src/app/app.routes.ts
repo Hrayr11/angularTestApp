660c6cfa03a48a3b7af387d78570f11c60f8d692
testappppp <poghosyan.hrayr1998@gmail.com>
2025-11-21 15:18:09 +0400

    added simple templates for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AuthComponent } from './auth/auth.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:  HomeComponent},   
+  { path: 'about', component: AboutComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'auth', component: AuthComponent } 
+];

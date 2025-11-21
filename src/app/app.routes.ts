0dd270b548fb03a12af626ef781cb2ca07788f9f
testappppp <poghosyan.hrayr1998@gmail.com>
2025-11-21 15:39:56 +0400

    added full height of pages via tailwind and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { AuthComponent } from './auth/auth.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component:  HomeComponent},   
   { path: 'about', component: AboutComponent },
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'auth', component: AuthComponent } 
+  { path: 'auth', component: AuthComponent } ,
+  { path: '**', component: NotFoundComponent }  
 ];

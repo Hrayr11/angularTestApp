6cbc891f4b1e44ecc9f263b472e0971f913edd7b
testappppp <poghosyan.hrayr1998@gmail.com>
2025-11-21 16:56:59 +0400

    added navbar and header component make some refactor and added tailwind config file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'app-header',
   imports: [RouterLink],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
   encapsulation: ViewEncapsulation.None
-}) 
-export class FooterComponent {
-
+})
+export class HeaderComponent {
+ 
 }
